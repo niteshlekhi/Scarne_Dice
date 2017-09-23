76e79db6daf79f1339235ba585f0706f8f30a920
niteshlekhi <niteshlekhi@gmail.com>
2017-09-23 22:14:18 +0530

    Main Game Working

--- app/src/main/java/lekhicomp/com/scarnesdice/MainActivity.java
+++ app/src/main/java/lekhicomp/com/scarnesdice/MainActivity.java
@@ -1,8 +1,16 @@
 package lekhicomp.com.scarnesdice;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
     int user_totalScore = 0;
     int comp_turnScore = 0;
     int comp_totalScore = 0;
-    int turn = 0;
+    int comp_roundScore=0;
+    int turn = 0;   //0 for User & 1 for Comp
+    int num = 0;
 
     @InjectView(R.id.userScore)
     TextView txtUser;
@@ -32,30 +42,21 @@ public class MainActivity extends AppCompatActivity {
     Button btnReset;
     @InjectView(R.id.imageDice)
     ImageView imgDice;
+    @InjectView(R.id.textNotify)
+    TextView txtNotify;
+
     Random random = new Random();
+    Handler handler = new Handler();
+    Bitmap bitmap;
+    Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-        btnRoll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int num = 0;
-                while (num != 1) {
-                    num = getRandomValue();
-                    Toast.makeText(MainActivity.this, "Number: " + num, Toast.LENGTH_SHORT).show();
-                    user_turnScore += num;
-                    updateUserScore();
-                }
-                if (num == 1) {
-                    Toast.makeText(MainActivity.this, "Computer's Turn", Toast.LENGTH_LONG).show();
-                    user_turnScore = 0;
-                    computerTurn();
-                }
-            }
-        });
+
+        userTurn();
 
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,66 +67,178 @@ public class MainActivity extends AppCompatActivity {
                 comp_totalScore = 0;
                 updateUserScore();
                 updateCompScore();
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice);
+                imgDice.setImageBitmap(bitmap);
+                userTurn();
             }
         });
-
-
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(1, 100, 0, "Two-Dice");
+        menu.add(1, 101, 0, "Fast Mode");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        Intent intent;
+        switch(id){
+            case 100:
+                intent=new Intent(MainActivity.this,TwoDice.class);
+                startActivity(intent);
+                break;
+
+            case 101:
+                intent=new Intent(MainActivity.this,FastDice.class);
+                startActivity(intent);
+                break;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void userTurn() {
+        btnRoll.setClickable(true);
+        btnHold.setClickable(false);
+        txtNotify.setText("Press ROLL to Roll the Dice or RESET to Reset the Game");
+        num = 0;
+        btnRoll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num = getRandomValue();
+
+                if (num != 1) {
+                    user_turnScore = num;
+                    updateUserScore();
+
+                } else if (num == 1) {
+                    user_turnScore=0;
+                    updateUserScore();
+                    computerTurn();
+                }
+
+                txtNotify.setText("Press ROLL to Roll the Dice or HOLD to End your Turn");
+                btnHold.setClickable(true);
+                btnHold.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        user_turnScore=0;
+                        updateUserScore();
+                        computerTurn();
+                    }
+                });
+
+
+            }
+        });
+    }
+
 
     //Method for Comp turn
     public void computerTurn() {
         btnRoll.setClickable(false);
         btnHold.setClickable(false);
-
-        while (turn == 1) {
-            int num = 0;
-
-            while (num != 1) {
+        txtNotify.setText("");
+        num = 0;
+        comp_roundScore=0;
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 num = getRandomValue();
-                Toast.makeText(MainActivity.this, "Number: " + num, Toast.LENGTH_SHORT).show();
-                comp_turnScore += num;
-                updateCompScore();
+
+                if (num != 1){
+                    Toast.makeText(MainActivity.this, "Number: " + num, Toast.LENGTH_SHORT).show();
+                    comp_turnScore = num;
+                    updateCompScore();
+                    if (comp_totalScore >= 100) {
+                        txtNotify.setText("Computer Won!!  Score:" + comp_totalScore);
+                        btnRoll.setClickable(false);
+                        btnHold.setClickable(false);
+                    }
+                    else
+                        computerTurn();
+
+                } else if (num == 1){
+                    Toast.makeText(MainActivity.this, "Player's Turn", Toast.LENGTH_LONG).show();
+                    comp_turnScore = 0;
+                    updateCompScore();
+                    userTurn();
+                }
             }
-            if (num == 1) {
-                Toast.makeText(MainActivity.this, "Player's Turn", Toast.LENGTH_LONG).show();
-                comp_turnScore = 0;
-            }
-        }
+        }, 2000);
     }
+
 
     //Method to get Random Dice value
     public int getRandomValue() {
         int num = random.nextInt(6) + 1;
         switch (num) {
             case 1:
-                imgDice.setImageResource(R.drawable.dice1);
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice1);
+                findViewById(R.id.imageDice).startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate));
+                imgDice.setImageBitmap(bitmap);
                 break;
             case 2:
-                imgDice.setImageResource(R.drawable.dice2);
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice2);
+                findViewById(R.id.imageDice).startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate));
+                imgDice.setImageBitmap(bitmap);
                 break;
             case 3:
-                imgDice.setImageResource(R.drawable.dice3);
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice3);
+                findViewById(R.id.imageDice).startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate));
+                imgDice.setImageBitmap(bitmap);
                 break;
             case 4:
-                imgDice.setImageResource(R.drawable.dice4);
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice4);
+                findViewById(R.id.imageDice).startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate));
+                imgDice.setImageBitmap(bitmap);
                 break;
             case 5:
-                imgDice.setImageResource(R.drawable.dice5);
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice5);
+                findViewById(R.id.imageDice).startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate));
+                imgDice.setImageBitmap(bitmap);
                 break;
             case 6:
-                imgDice.setImageResource(R.drawable.dice6);
-                break;
+                bitmap = BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.dice6);
+                findViewById(R.id.imageDice).startAnimation(AnimationUtils.loadAnimation(this,R.anim.rotate));
+                imgDice.setImageBitmap(bitmap);
         }
         return num;
     }
 
     public void updateUserScore() {
-        user_totalScore += user_turnScore;
+        if (user_totalScore >= 100) {
+            txtNotify.setText("User Won!!  Score:" + user_totalScore);
+            btnRoll.setClickable(false);
+            btnHold.setClickable(false);
+
+        }
+
+        if(user_turnScore!=1)
+            user_totalScore += user_turnScore;
+        else {
+            user_turnScore=0;
+            btnRoll.setClickable(false);
+            Toast.makeText(MainActivity.this, "Computer's Turn", Toast.LENGTH_LONG).show();
+        }
         txtUser.setText("Your Score: " + user_totalScore);
+
+
     }
 
     public void updateCompScore() {
-        comp_totalScore += comp_turnScore;
+
+        if(comp_turnScore!=1)
+            comp_totalScore += comp_turnScore;
+        else{
+            comp_turnScore=0;
+        }
         txtComp.setText("Computer Score: " + comp_totalScore);
     }
+
 }
